85b3689d6e2178cf2361ecca1ac274e05483417d
Jesse Williams <jessenw3@gmail.com>
2022-12-07 23:13:10 +1300

    Day02B working with real input

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,5 +1,3 @@
-import java.io.File
-
 fun main() {
     fun part1(input: List<String>) =
         input
@@ -10,34 +8,38 @@ fun main() {
                     if (round.first == round.second) 3
                     else if (round.second == 1 && round.first == 3
                         || round.second == 3 && round.first == 2
-                        || round.second == 2 && round.first == 1) {
-                        6
-                    }
-                    else {
-                        0
-                    }
+                        || round.second == 2 && round.first == 1) 6
+                    else 0
                 roundPoints + round.second
             }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<String>) =
+        input
+            .sumOf {
+                val split = it.split(" ")
+                val round = Pair(scoreForPlay(split[0]), scoreForPlay(split[1]))
+
+                if (round.second == 1)
+                    if (round.first - 1 < 1) 3 else round.first - 1
+                else if (round.second == 2)
+                    round.first + 3
+                else
+                    (if (round.first + 1 > 3) 1 else round.first + 1) + 6
+            }
 
     val testInput = readInputLines("Day02_test")
-    println(part1(testInput))
-    println(part2(testInput))
     check(part1(testInput) == 15)
-//    check(part2(testInput) == 45000)
+    check(part2(testInput) == 12)
 
     val input = readInputLines("Day02")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
 
 fun scoreForPlay(play: String) =
     when (play) {
-        "A", "X" -> 1 // Rock
-        "B", "Y" -> 2 // Paper
-        "C", "Z" -> 3 // Scissors
+        "A", "X" -> 1 // Rock, lose
+        "B", "Y" -> 2 // Paper, draw
+        "C", "Z" -> 3 // Scissors, win
         else -> 0
     }
