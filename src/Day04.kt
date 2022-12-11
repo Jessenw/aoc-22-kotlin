85843e88921aefd3eeb6565541f76d1fa76f9dc9
Jesse Williams <jessenw3@gmail.com>
2022-12-11 20:05:54 +1300

    Day4b solved!

--- src/Day04.kt
+++ src/Day04.kt
@@ -14,17 +14,34 @@ fun main() {
                 else 0
         }.sum()
 
-    fun part2(input: List<String>) {
+    fun part2(input: List<String>): Int {
+        return input.map { pair ->
+            val sections =
+                pair.split(",")
+                    .map { ranges ->
+                        ranges.split("-")
+                            .map { it.toInt() }
+                    }
+                    .map {
+                        // Generate range of values with given start and end
+                        (it[1] downTo it[0]).toList()
+                    }
+                    .flatten()
 
+            // If we remove duplicates, then there is overlap
+            if (sections.distinct().size != sections.size) 1
+            else 0
+        }.sum()
     }
 
     val testInput = readInputLines("Day04_test")
-    check(part1(testInput) == 3)
-//    check(part2(testInput) == 12)
+    part2(testInput)
+    check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
 
     val input = readInputLines("Day04")
     check(part1(input) == 413)
-//    println(part2(input))
+    check(part2(input) == 806)
 }
 fun rangeInRange(a: Pair<Int, Int>, b: Pair<Int, Int>) =
     a.first >= b.first && a.second <= b.second
