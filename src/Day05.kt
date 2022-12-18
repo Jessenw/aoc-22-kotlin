a88a6ea1661e2ebf1f6e654c6ed06e344298a615
Jesse Williams <jessenw3@gmail.com>
2022-12-18 17:25:19 +1300

    Day05 - Build stacks

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,14 +1,53 @@
+import java.io.File
+
+
+fun buildStacks(lines: List<String>): List<ArrayDeque<String>> {
+    val stackCount = lines
+        .dropWhile { it.contains("[") }
+        .first()
+        .split(" ")
+        .filter { it.isNotBlank() }
+        .maxOf { it.toInt() }
+
+    val stacks = MutableList(stackCount) { ArrayDeque<String>() }
+
+    for (i in 0..stackCount) {
+        lines
+            .dropLast(1)
+            .map {
+                it
+                    .chunked(1)
+                    .toMutableList()
+                    .drop(1)
+            }
+            .forEach {
+                val index = i * 4
+                if (index <= it.size) {
+                    val element = it[index]
+                    if (element != " ") {
+                        // These are "stacks" but we add as a queue since we read lines top to bottom
+                        stacks[i].addFirst(element)
+                    }
+                }
+            }
+    }
+
+    return stacks
+}
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(stacks: List<ArrayDeque<String>>, moves: String): String {
+
+        return ""
     }
 
     fun part2(input: List<String>) {
 
     }
 
-    val testInput = readInputLines("Day03_test")
-    check(part1(testInput) == 15)
+    val file = File("input", "Day05_test.txt").readText()
+    val splitFile = file.split("\n\n")
+    check(part1(buildStacks(splitFile[0].lines()), splitFile[1]) == "CMZ")
 //    check(part2(testInput) == 12)
 //
 //    val input = readInputLines("Day03")
