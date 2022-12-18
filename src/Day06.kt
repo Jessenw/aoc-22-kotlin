776ca684fdd27d516ed06d1073f3225f7644accd
Jesse Williams <jessenw3@gmail.com>
2022-12-18 19:16:47 +1300

    Day06a solved!

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,17 +1,29 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: String): Int {
+        val buffer = ArrayDeque<Char>()
+        val monkey = input
+            .toCharArray()
+            .indexOfFirst { element ->
+                buffer.addLast(element)
+                if (buffer.size > 4) buffer.removeFirst()
+                if (buffer.size > 3) return@indexOfFirst (buffer.distinct().size > 3)
+
+                return@indexOfFirst false
+            }
+        return monkey + 1
     }
 
     fun part2(input: List<String>) {
 
     }
 
-    val testInput = readInputLines("Day03_test")
-    check(part1(testInput) == 15)
+    check(part1("bvwbjplbgvbhsrlpgdmjqwftvncz") == 5)
+    check(part1("nppdvjthqldpwncqszvftbrmjlhg") == 6)
+    check(part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 10)
+    check(part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 11)
 //    check(part2(testInput) == 12)
-//
-//    val input = readInputLines("Day03")
-//    println(part1(input))
+
+    val input = readInputText("Day06")
+    println(part1(input))
 //    println(part2(input))
 }
