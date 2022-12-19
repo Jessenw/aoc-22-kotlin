ddbdf591095c87669c4cfac50d061ffad45e3965
Jesse Williams <jessenw3@gmail.com>
2022-12-19 22:59:12 +1300

    Day07b solved!

--- src/Day07.kt
+++ src/Day07.kt
@@ -25,6 +25,22 @@ fun deleteCandidates(
     return list.toList()
 }
 
+fun deleteCandidates2(
+    currentNode: Node<ItemType.Directory>,
+    limit: Int,
+    list: MutableList<Int> = mutableListOf()
+) : List<Int> {
+    val size = currentNode.calculateSize()
+    if (size > limit)
+        list.add(size)
+
+    currentNode.children.forEach {
+        deleteCandidates2(it, limit, list)
+    }
+
+    return list.toList()
+}
+
 sealed class ItemType(open val name: String) {
     data class File(
         override val name: String,
@@ -93,7 +109,55 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val root = Node(ItemType.Directory("/"))
+        val currentPath = ArrayDeque<String>().also { it.add(root.value.name) }
+
+        var inputIndex = 0
+        while (inputIndex in input.indices) {
+            val currentLine = input[inputIndex]
+            val tokens = currentLine.split(" ").toMutableList()
+
+            // Commands begin with "$"
+            if (tokens.first() == "$") {
+                tokens.removeFirst()
+
+                when(tokens.removeFirst()) {
+                    "cd" -> {
+                        when (val path = tokens.first()) {
+                            ".." -> currentPath.removeLast()
+                            "/" -> { }
+                            else -> currentPath.addLast(path)
+                        }
+                        inputIndex++
+                    }
+                    "ls" -> {
+                        // Get all files output from ls (until we hit the next command)
+                        inputIndex++ // Skip to next line
+                        while(inputIndex < input.size && !input[inputIndex].contains("$")) {
+                            val lsSplit = input[inputIndex].split(" ")
+
+                            val currentDirectory = findDirectory(root, currentPath)
+                            if (lsSplit.first() == "dir")
+                                currentDirectory.children.add(Node(ItemType.Directory(lsSplit[1])))
+                            else
+                                currentDirectory.value.files.add(ItemType.File(lsSplit[1], lsSplit[0].toInt()))
+
+                            inputIndex++
+                        }
+                    }
+                }
+            }
+        }
+
+        val diskSize = 70000000
+        val updateSize = 30000000
+        val usedSpace = root.calculateSize()
+        val unusedSpace = diskSize - usedSpace
+        val storageRequired = updateSize - unusedSpace
+
+        val hm = deleteCandidates2(root, storageRequired)
+        val min = hm.minOf { it }
+        return min
     }
 
     val testInput = readInputLines("Day07_test")
@@ -102,5 +166,5 @@ fun main() {
 
     val input = readInputLines("Day07")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
